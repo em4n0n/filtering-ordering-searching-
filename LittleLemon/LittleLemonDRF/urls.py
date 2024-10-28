e198d0021bb2e347ef3775e624eaf410308b5963
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-27 22:18:26 -0700

    Generate token from api endpoint

--- LittleLemon/LittleLemonDRF/urls.py
+++ LittleLemon/LittleLemonDRF/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    # path('categories', views.CategoriesView.as_view()),
-    # path('menu-items', views.MenuItemsView.as_view()),
+    path('categories', views.CategoriesView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
     path('secret/', views.secret),
+    path('api-token-auth/', obtain_auth_token)
 ]
