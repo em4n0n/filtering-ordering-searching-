d4c3d6f1984aada5f14b30145f78c97415f36bbe
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-27 22:09:41 -0700

    Uncomment code

--- LittleLemon/LittleLemonDRF/urls.py
+++ LittleLemon/LittleLemonDRF/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories', views.CategoriesView.as_view()),
-    path('menu-items', views.MenuItemsView.as_view()),
-    path('secret/'. views.secret),
+    # path('categories', views.CategoriesView.as_view()),
+    # path('menu-items', views.MenuItemsView.as_view()),
+    path('secret/', views.secret),
 ]
--- LittleLemon/LittleLemonDRF/views.py
+++ LittleLemon/LittleLemonDRF/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from rest_framework import generics
+# from .models import MenuItem, Category
+# from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# class CategoryView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+    
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+#     ordering_fields = ['title', 'price']
+#     filterset_fields = ['title', 'price']
+#     search_fields = ['title']
+    
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message":"Some secret message"})
